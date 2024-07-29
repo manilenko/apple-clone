0e5061549152c412d45a259bcca1d57d9c58d964
JuliaAnat <107546189+JuliaAnat@users.noreply.github.com>
2024-07-28 20:15:34 -0400

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
 
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
+  
   return (
-    <>
-   
-    </>
-  )
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 }
 
 export default App
